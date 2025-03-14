e23c6533f691bfc7dc7133638beda31f46c758ba
Namic24 <Artitaya.nam1@gmail.com>
2025-03-14 11:48:28 +0700

    test

---
+++ app/(tabs)/bookmark.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function TabBookMark() {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+
+    <View style={{ flex: 1 , justifyContent:'center' , alignItems:'center' }}>
+      <Text>bookmark</Text>
+    </View>
+
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function TabProfile() {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+
+    <View style={{ flex: 1 , justifyContent:'center' , alignItems:'center' }}>
+      <Text>profile</Text>
+    </View>
+
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({})
