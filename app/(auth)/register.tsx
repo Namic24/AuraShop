ddc066a3e3504f1b2184e2f8372c9619fa68e145
Namic24 <Artitaya.nam1@gmail.com>
2025-03-17 10:43:34 +0700

    10/03

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,13 +1,61 @@
-import { View, Text, ScrollView, Image, Dimensions } from 'react-native'
+import { View, Text, ScrollView, Image, Dimensions, KeyboardAvoidingView, Platform, Alert } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import FormField from '@/components/FromField'
 import CustomButton from '@/components/CustomButtom'
-import { Link } from 'expo-router'
+import {  router } from 'expo-router'
 import { images } from '@/constants'
+import Button from '@/components/Button'
+import { useState } from 'react'
+import { supabase } from '@/utils/supabase'
 
 export default function register() {
+
+  const [displayName, setDisplayName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const handleRegister = async () => {
+    setError('')
+
+    if(!displayName || !email || !password) {
+      Alert.alert('Incomplete Information', 'Please fill in all fields')
+      return
+    }
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: { 
+          displayName 
+        }
+      }
+    })
+
+    if(error) {
+      setError(error.message)
+    } else {
+      console.log('Token:', data.session?.access_token) // แสดง token ไปยัง console
+      Alert.alert(
+        'Registration Successful',
+        'You have successfully registered. Press OK to login.',
+        [
+          {
+            text: 'OK',
+            onPress: () => router.replace('/login'), // ส่งไปยังหน้า Login
+          },
+        ]
+      )
+    }
+  }
+
   return (
     <SafeAreaView className="bg-gray-900 h-full px-4 ">
+       <KeyboardAvoidingView
+              behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+              className='h-full'
+            >
       <ScrollView>
         <View
           className="w-full flex justify-center h-full px-5 py-5"
@@ -15,38 +63,49 @@ export default function register() {
             minHeight: Dimensions.get("window").height - 100,
           }}
         >
+           <View className='flex items-center'>
           <Image
             source={images.logo}
             resizeMode="contain"
             className="h-[34px]"
           />
+          </View>
 
           <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
             Sign Up to AuraShop
           </Text>
 
           <FormField
-            title="Username"
-            value={null}
+            title="Display Name"
+            value={displayName}
+            onChangeText={setDisplayName}
             otherStyles="mt-10"
           />
 
           <FormField
             title="Email"
-            value={null}
+            value={email}
+            onChangeText={setEmail}
             otherStyles="mt-7"
             keyboardType="email-address"
           />
 
           <FormField
             title="Password"
-            value={null}
+            value={password}
+            onChangeText={setPassword}
             otherStyles="mt-7"
           />
 
+            {
+              error ? (
+                <Text className="text-red-500 mt-4">{error}</Text>
+              ) : null
+            }
+
           <CustomButton
             title="Register"
-            handlePress={() => {}}
+            handlePress={handleRegister}
             containerStyles="mt-7"
           />
 
@@ -54,15 +113,11 @@ export default function register() {
             <Text className="text-lg text-gray-100 font-pregular">
               Have an account already?
             </Text>
-            <Link
-              href="/login"
-              className="text-lg font-psemibold text-orange-500 text-secondary"
-            >
-              Login
-            </Link>
+            <Button title="Login" onPress={() => router.replace('/login')}/>
           </View>
         </View>
       </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   )
 }
